2ebe588fd4c53f5daa714f3da77d9500c935ea65
Lin Fang Yi <76465584+yiyiscode@users.noreply.github.com>
2023-12-10 18:28:14 +0800

    test

--- dashboard/pages/ConfusionMatrix.py
+++ dashboard/pages/ConfusionMatrix.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 tab1, tab2 = st.tabs(["Confusion Matrix", "NormalizedConfusion Matrix"])
-tab1.image(r"../runs/detect/train15/confusion_matrix.png")
-tab2.image(r"../runs/detect/train15/confusion_matrix_normalized.png")
+tab1.image(r"../../runs/detect/train15/confusion_matrix.png")
+tab2.image(r"../../runs/detect/train15/confusion_matrix_normalized.png")
