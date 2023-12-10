8512f3a33097d3784136d5ce73a6c99e94a3fc6e
Lin Fang Yi <76465584+yiyiscode@users.noreply.github.com>
2023-12-10 18:09:29 +0800

    Delete Curve.py

--- Curve.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-tab1, tab2, tab3, tab4 = st.tabs(["F1", "Precision", "Precision/Recall", "Recall"])
-tab1.image(r"../runs/detect/train15/F1_curve.png")
-tab2.image(r"../runs/detect/train15/P_curve.png")
-tab3.image(r"../runs/detect/train15/PR_curve.png")
-tab4.image(r"../runs/detect/train15/R_curve.png")
