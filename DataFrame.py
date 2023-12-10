10b614b9a19e494b3342b8a3a8514463a2ff26b8
Lin Fang Yi <76465584+yiyiscode@users.noreply.github.com>
2023-12-10 18:09:45 +0800

    Delete DataFrame.py

--- DataFrame.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.title("Results")
-
-df = pd.read_csv(r"../runs/detect/train15/results.csv", index_col=0)
-st.dataframe(df.style.highlight_min(axis=0))
