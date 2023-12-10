befd786849e1652dfc0acc9e3b5e5433b22a7843
Lin Fang Yi <76465584+yiyiscode@users.noreply.github.com>
2023-12-10 18:09:18 +0800

    Delete ConfusionMatrix.py

--- ConfusionMatrix.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-tab1, tab2 = st.tabs(["Confusion Matrix", "NormalizedConfusion Matrix"])
-tab1.image(r"../runs/detect/train15/confusion_matrix.png")
-tab2.image(r"../runs/detect/train15/confusion_matrix_normalized.png")
